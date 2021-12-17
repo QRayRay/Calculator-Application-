f5ad271beb794d200f2f6313d4854f8859c2a12d
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2021-12-16 21:21:20 -0600

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -13,6 +13,8 @@ private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 class MainActivity : AppCompatActivity() {
 
     // Variables to hold the operands and type of calculation
+    
+    // Private var to set the buttons
     private var operand1: Double? = null
     private var pendingOperation = "="
 
