eff20b3129dafcfce350ba91b205cd63e412973a
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-02-28 19:12:46 -0600

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,18 +1,19 @@
-<?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".MainActivity">
+package com.example.mycalculator
 
-    <TextView
-        android:layout_width="match_parent"
-        android:layout_height="250dp"
-        android:background="#efefef"
-        android:padding="10dp"
-        android:textSize="48sp"
-        android:text="@string/_12"
-        android:gravity="right |bottom"/>
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-</LinearLayout>
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText( this, "Button clicked", Toast.LENGTH_LONG).show()
+    }
+
+
+}
