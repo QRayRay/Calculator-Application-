22a9eb59dc8c1a53221b5796713decbcf67c067e
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-03-01 20:21:37 -0600

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,17 +3,29 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+
     }
 
-    fun onDigit(view: View){
-        Toast.makeText( this, "Button clicked", Toast.LENGTH_LONG).show()
+
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
     }
 
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
 
 }
