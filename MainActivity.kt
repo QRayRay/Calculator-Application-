9f03c5764c7cf409951e72d66369871ff7f58c1e
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-03-14 19:37:54 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -8,9 +8,12 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // The code needs t obe insteted to make sure that decimals can be used properly during the app run time.
-    
     private var tvInput: TextView? = null
+
+    // dot is now treated as a dot instead of as a number. This due to the boolean value being false.
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,4 +26,28 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
+
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
+
+
+    // execute code to append now and Flags, it will help us know what is active.
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+// To adda prefix, select options after value and enter in ""
+    private fun isOperartorAdded(value : String) : Boolean {
+        return if(value.startsWith("-")){
+            false
+        }
+    }
+}
